800cabee93385ded2563ff00049231e8799dcd29
Artem <40538552+surge603@users.noreply.github.com>
2022-02-20 13:31:34 +0300

    Задание 10

--- tsk10-second_digit/Program.cs
+++ tsk10-second_digit/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Показать вторую цифру трёхзначного числа.
+
+int num = new Random().Next(100,1000);
+Console.Clear();
+Console.WriteLine ($"Вторая цифра числа {num} - это " + num%100/10);
